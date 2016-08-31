3ba6b590f40592bcf308e05e72f5ab58403e2673
Andrew Louis <andrew.louis@shopify.com>
2016-08-31 11:29:26 -0400

    validated

--- lib/record_store/record/alias.rb
+++ lib/record_store/record/alias.rb
@@ -1,20 +1,20 @@
 module RecordStore
   class Record::ALIAS < Record
-    attr_accessor :t
+    attr_accessor :alias_value
 
-    validates :t, presence: true, format: { with: Record::CNAME_REGEX, message: 'is not a fully qualified domain name' }
+    validates :alias_value, presence: true, format: { with: Record::CNAME_REGEX, message: 'is not a fully qualified domain name' }
 
     def initialize(record)
       super
-      @t = Record.ensure_ends_with_dot(record.fetch(:t))
+      @alias_value = Record.ensure_ends_with_dot(record.fetch(:alias))
     end
 
     def rdata
-      { t: t }
+      { alias: alias_value }
     end
 
     def to_s
-      "[ALIASRecord] #{fqdn} #{ttl} IN ALIAS #{t}"
+      "[ALIASRecord] #{fqdn} #{ttl} IN ALIAS #{alias_value}"
     end
   end
 end
