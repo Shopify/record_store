034b16b6912aa95383334db3eb3ba5cc975d8b7f
S. Brent Faulkner <brent.faulkner@shopify.com>
2018-03-31 23:04:34 -0400

    add deprecation message for SPF record

--- lib/record_store/record/spf.rb
+++ lib/record_store/record/spf.rb
@@ -1,4 +1,8 @@
 module RecordStore
   class Record::SPF < Record::TXT
+    def initialize(record)
+      STDERR.puts "SPF record type is deprecated (See RFC 7208 Section 14.1)"
+      super
+    end
   end
 end
