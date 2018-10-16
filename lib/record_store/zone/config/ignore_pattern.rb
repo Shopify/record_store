86c162267969683c3d75da7a6741582bb37b00a0
Chris Taylor <chris.taylor@shopify.com>
2018-10-16 16:28:31 -0400

    Restrict the interface of IgnorePattern

--- lib/record_store/zone/config/ignore_pattern.rb
+++ lib/record_store/zone/config/ignore_pattern.rb
@@ -2,9 +2,6 @@ module RecordStore
   class Zone
     class Config
       class IgnorePattern
-        attr_accessor :orig_hash
-        alias_method :to_hash, :orig_hash
-
         def initialize(orig_hash)
           @orig_hash = orig_hash
         end
@@ -12,6 +9,14 @@ module RecordStore
         def should_ignore?(record)
           all_pairs.all?{|(key, value)| record.respond_to?(key) && value_matches?(value, record.send(key))}
         end
+
+        def to_hash
+          orig_hash
+        end
+
+        private
+
+        attr_accessor :orig_hash
 
         def all_pairs
           orig_hash.reject{|key, value| key === 'match'}
