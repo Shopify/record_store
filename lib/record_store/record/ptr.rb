8519d7a5aad4cabdf7489608fbe0f5a00ac53965
Ryan De Villa <ryan.devilla@shopify.com>
2020-04-15 20:51:51 -0400

    Remove obsolete validation

--- lib/record_store/record/ptr.rb
+++ lib/record_store/record/ptr.rb
@@ -3,7 +3,6 @@ module RecordStore
     attr_accessor :ptrdname
 
     validate :validate_fqdn_inside_in_addr_arpa_zone
-    validate :validate_ptrdname_is_rooted
 
     def initialize(record)
       super
@@ -21,10 +20,6 @@ module RecordStore
 
     def validate_fqdn_inside_in_addr_arpa_zone
       errors.add(:fqdn, 'must be in the `in-addr.arpa.` zone') unless fqdn.end_with?('in-addr.arpa.')
-    end
-
-    def validate_ptrdname_is_rooted
-      errors.add(:ptrdname, 'ptrdname must be explicitly rooted') unless ptrdname.end_with?('.')
     end
   end
 end
