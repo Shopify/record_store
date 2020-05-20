9b25ed0663b19423527b5a8dc32ac1ce23d3c7ab
S. Brent Faulkner <brent.faulkner@shopify.com>
2020-05-20 12:05:29 -0400

    remove old commented out tests

--- test/cli/validate_authority_test.rb
+++ test/cli/validate_authority_test.rb
@@ -160,29 +160,5 @@ module CLI
     def ns_unknown(fqdn)
       ns_records(fqdn: fqdn, nsdomain: 'charlestonroadregistry.com', count: 5)
     end
-
-    # def test_lists_providers
-    #   RecordStore::CLI.start(%w(info))
-
-    #   providers = <<~PROVIDERS
-    #     Providers:
-    #     - DNSimple
-    #     - NS1
-    #   PROVIDERS
-
-    #   assert_includes($stdout.string, providers)
-    # end
-
-    # def test_lists_authoritative_nameservers
-    #   RecordStore::CLI.start(%w(info))
-
-    #   authority = <<~AUTHORITY
-    #     Authoritative nameservers:
-    #     - [NSRecord] example.com. 172800 IN NS a.iana-servers.net.
-    #     - [NSRecord] example.com. 172800 IN NS b.iana-servers.net.
-    #   AUTHORITY
-
-    #   assert_includes($stdout.string, authority)
-    # end
   end
 end
