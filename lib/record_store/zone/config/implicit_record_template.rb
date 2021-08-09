d46e1cc75b118c47bb3c45a1a36a4c5446b737c9
Mathusan Selvarajah <mathusan.selvarajah@shopify.com>
2021-08-09 11:35:52 -0400

    Initialize variables if the filters don't exist in the template file

--- lib/record_store/zone/config/implicit_record_template.rb
+++ lib/record_store/zone/config/implicit_record_template.rb
@@ -28,8 +28,8 @@ module RecordStore
             template_file = File.read(filepath)
 
             template_file_yaml = YAML.load(template_file).deep_symbolize_keys
-            filters_for_records_to_template = template_file_yaml[:each_record]
-            filters_for_records_to_exclude = template_file_yaml[:except_record]
+            filters_for_records_to_template = template_file_yaml[:each_record] || []
+            filters_for_records_to_exclude = template_file_yaml[:except_record] || []
 
             new(template: ERB.new(template_file),
                 filters_for_records_to_template: filters_for_records_to_template,
