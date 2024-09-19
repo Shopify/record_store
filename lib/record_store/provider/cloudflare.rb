8b3bba8936af48a0f8861b68a1904a5b89d47d0c
Thomas Antonio <thomas.antonio@shopify.com>
2024-09-19 10:01:31 -0400

    Create build_api_body

--- lib/record_store/provider/cloudflare.rb
+++ lib/record_store/provider/cloudflare.rb
@@ -44,20 +44,61 @@ module RecordStore
 
       private
 
-      def add(record)
-        # Implementation for adding a record to the provider's zone
+      def add(record, zone)
+        zone_id = zone_name_to_id(zone)
+
+        api_record = build_api_body(record)
+
+        retry_on_connection_errors do
+          client.post("/client/v4/zones/#{zone_id}/dns_records", api_record)
+        end
       end
 
-      def remove(record)
-        # Implementation for removing a record from the provider's zone
+      def remove(record, zone)
+        zone_id = zone_name_to_id(zone)
+
+        retry_on_connection_errors do
+          client.delete("/client/v4/zones/#{zone_id}/dns_records/#{record.id}")
+        end
       end
 
-      def update(record)
+      def update(record, zone)
         # Implementation for updating a record in the provider's zone
       end
 
       def secrets
         super.fetch('cloudflare')
+      end
+
+      def build_api_body(record)
+        api_record = {
+          comment: '',
+          name: record[:fqdn],
+          proxied: false,
+          settings: {},
+          tags: record[:tags] || [],
+          ttl: record[:ttl] || 1,
+          type: record[:type],
+        }
+        case record
+        when Record::A, Record::AAAA
+          api_record[:content] = record.rdata_txt
+        when Record::CNAME
+          api_record[:content] = record.rdata_txt
+        when Record::PTR
+          api_record[:content] = record.rdata_txt
+        when Record::MX
+          api_record[:content] = record.exchange
+          api_record[:priority] = record.preference
+        when Record::TXT, Record::SPF
+          api_record[:content] = record.txtdata.gsub('\;', ';')
+        when Record::CAA
+          api_record[:data] = record.rdata
+        when Record::SRV
+          api_record[:data] = rdata
+        end
+
+        api_record
       end
 
       # Not required but used in proctical implementations of retrieve_current_records
@@ -95,11 +136,12 @@ module RecordStore
           weight, port, host = api_record['content'].split(' ')
 
           record.merge!(
-            priority: api_record['priority'],
+            priority: api_record['priority'].to_i,
             weight: weight.to_i,
             port: port.to_i,
             target: Record.ensure_ends_with_dot(host),
           )
+          # TODO: How to build ALIAS?
         end
 
         Record.const_get(record_type).new(record)
@@ -111,6 +153,18 @@ module RecordStore
           secrets['api_token'],
           secrets['account_id'],
         )
+      end
+
+      def zone_name_to_id(zone_name)
+        retry_on_connection_errors do
+          zone_id = nil
+          client.get('/client/v4/zones').result_raw.each do |zone|
+            zone_id = zone['id'] if zone['name'] == zone_name
+          end
+          raise "Zone not found: #{zone_name}" unless zone_id
+
+          zone_id
+        end
       end
     end
   end
